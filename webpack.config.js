cd3113b747c8a8fbd1bf3a384d43b06387f0f2d8
Samuel Castillo <73082164+samuelcastillogt@users.noreply.github.com>
2021-06-10 19:46:29 -0600

    Merge

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
  const CopyWebpackPlugin = require('copy-webpack-plugin')
@@ -39,4 +40,47 @@ module.exports = {
             ]
         })
     ]
+=======
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+ const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    resolve: {
+        extensions: ['.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            }
+        ]
+    },
+   plugins: [
+        new HtmlWebpackPlugin(
+            {
+                inject: true,
+                template: './public/index.html',
+                filename: './index.html',
+            }
+        ),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: './src/style/style.css',
+                    to: '.'
+                }
+            ]
+        })
+    ]
+>>>>>>> a478405e51524987bb2f7c1b9eb61bc4bbcae356
 }
